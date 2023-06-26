d68b72acb35f4c8aabbad72e8ad11398b518ce07
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-06-26 20:42:44 +0200

    Modified

--- api/app.py
+++ api/app.py
@@ -16,30 +16,27 @@ def hello():
     return 'Hello, World!'
 
 @app.route('/wine', methods=["GET"])
-def predictionWine():
-    try:
-        if request.method == 'GET':
-            alcohol = float(request.args.get('alcohol'))
-            malic_acid = float(request.args.get('malic_acid'))
-            ash = float(request.args.get('ash'))
-            alcalinity = float(request.args.get('alcalinity_of_ash'))
-            magnesium = float(request.args.get('magnesium'))
-            phenos = float(request.args.get('total_phenos'))
-            flavanoids = float(request.args.get('flavanoids'))
-            nonflavanoid = float(request.args.get('nonflavanoid_phenols'))
-            proanthocyanins = float(request.args.get('proanthocyanins'))
-            intensity = float(request.args.get('color_intensity'))
-            hue = float(request.args.get('hue'))
-            od315 = float(request.args.get('od315_of_diluted_wines'))
-            proline = float(request.args.get('proline'))
+def prediction_wine():
+    if request.method == 'GET':
+        alcohol = float(request.args.get('alcohol'))
+        malic_acid = float(request.args.get('malic_acid'))
+        ash = float(request.args.get('ash'))
+        alcalinity = float(request.args.get('alcalinity_of_ash'))
+        magnesium = float(request.args.get('magnesium'))
+        phenos = float(request.args.get('total_phenos'))
+        flavanoids = float(request.args.get('flavanoids'))
+        nonflavanoid = float(request.args.get('nonflavanoid_phenols'))
+        proanthocyanins = float(request.args.get('proanthocyanins'))
+        intensity = float(request.args.get('color_intensity'))
+        hue = float(request.args.get('hue'))
+        od315 = float(request.args.get('od315_of_diluted_wines'))
+        proline = float(request.args.get('proline'))
 
-            final_features = ([[alcohol, malic_acid, ash, alcalinity, magnesium, phenos, flavanoids, nonflavanoid, proanthocyanins, intensity, hue, od315, proline]])
+        final_features = ([[alcohol, malic_acid, ash, alcalinity, magnesium, phenos, flavanoids, nonflavanoid, proanthocyanins, intensity, hue, od315, proline]])
 
-            prediction = model.predict(final_features)
+        prediction = model.predict(final_features)
         
-        return jsonify(str(prediction[0]))
-    except:
-        print('Something went wrong.')
+    return jsonify(str(prediction[0]))
  
 if __name__ == '__main__':
     app.run()

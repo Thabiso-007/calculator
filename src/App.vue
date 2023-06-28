eb7324d77096678a191ebffea4343157f3268769
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-06-28 13:44:17 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Header msg="Basic Calculators"/>
+  <Calculator />
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

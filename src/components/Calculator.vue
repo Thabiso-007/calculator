835d176cfc19f602e98e178917a995dd6e730580
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2023-06-29 13:15:20 +0200

    Added calculator functionality

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -7,12 +7,12 @@
                         <tbody class="table-group-divider mt-5">
                             <tr class="output">
                                 <td class="p-4" colspan="4">
-                                    0
+                                    {{ output }}
                                 </td>
                             </tr>
                             <tr>
-                                <td class="p-4">Del</td>
-                                <td class="p-4">
+                                <td class="p-4 cursor-pointer" v-on:click="clearField">Del</td>
+                                <td class="p-4 cursor-pointer" v-on:click="setNegativeOrPositive">
                                     <svg 
                                         xmlns="http://www.w3.org/2000/svg" 
                                         width="1em" 
@@ -26,7 +26,7 @@
                                         </path>
                                     </svg>
                                 </td>
-                                <td class="p-4">
+                                <td class="p-4 cursor-pointer" v-on:click="calculatePercentage">
                                     <svg 
                                         xmlns="http://www.w3.org/2000/svg" 
                                         width="1em" 
@@ -39,7 +39,7 @@
                                         </path>
                                     </svg>
                                 </td>
-                                <td class="p-3">
+                                <td class="p-3 cursor-pointer" v-on:click="processOutput('divide')">
                                     <svg 
                                         xmlns="http://www.w3.org/2000/svg" 
                                         width="1em" 
@@ -53,51 +53,51 @@
                                         </path>
                                     </svg>
                                 </td>
-                              </tr>
-                          <tr>
-                            <td class="p-4">7</td>
-                            <td class="p-4">8</td>
-                            <td class="p-4">9</td>
-                            <td class="p-4">
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    width="1em" height="1em" 
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path 
-                                        fill="currentColor" 
-                                        fillRule="evenodd" 
-                                        d="M4.293 4.293a1 1 0 0 1 1.414 0L10 8.586l4.293-4.293a1 1 0 1 1 1.414 1.414L11.414 10l4.293 4.293a1 1 0 0 1-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 0 1-1.414-1.414L8.586 10L4.293 5.707a1 1 0 0 1 0-1.414Z" 
-                                        clipRule="evenodd"
+                            </tr>
+                            <tr>
+                                <td class="p-4 cursor-pointer" v-on:click="getNumber('7')">7</td>
+                                <td class="p-4 cursor-pointer" v-on:click="getNumber('8')">8</td>
+                                <td class="p-4 cursor-pointer" v-on:click="getNumber('9')">9</td>
+                                <td class="p-4 cursor-pointer" v-on:click="processOutput('multiply')">
+                                    <svg 
+                                        xmlns="http://www.w3.org/2000/svg" 
+                                        width="1em" height="1em" 
+                                        viewBox="0 0 20 20"
                                     >
-                                    </path>
-                                </svg>
+                                        <path 
+                                            fill="currentColor" 
+                                            fillRule="evenodd" 
+                                            d="M4.293 4.293a1 1 0 0 1 1.414 0L10 8.586l4.293-4.293a1 1 0 1 1 1.414 1.414L11.414 10l4.293 4.293a1 1 0 0 1-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 0 1-1.414-1.414L8.586 10L4.293 5.707a1 1 0 0 1 0-1.414Z" 
+                                            clipRule="evenodd"
+                                        >
+                                        </path>
+                                    </svg>
                             </td>
-                          </tr>
-                          <tr>
-                            <td class="p-4">4</td>
-                            <td class="p-4">5</td>
-                            <td class="p-4">6</td>
-                            <td class="p-4">
-                                <svg 
-                                    xmlns="http://www.w3.org/2000/svg" 
-                                    width="1em" 
-                                    height="1em" 
-                                    viewBox="0 0 1024 1024"
-                                >
-                                    <path 
-                                        fill="currentColor" 
-                                        d="M128 544h768a32 32 0 1 0 0-64H128a32 32 0 0 0 0 64z"
+                            </tr>
+                            <tr>
+                                <td class="p-4 cursor-pointer" v-on:click="getNumber('4')">4</td>
+                                <td class="p-4 cursor-pointer" v-on:click="getNumber('5')">5</td>
+                                <td class="p-4 cursor-pointer" v-on:click="getNumber('6')">6</td>
+                                <td class="p-4 cursor-pointer" v-on:click="processOutput('subtract')">
+                                    <svg 
+                                        xmlns="http://www.w3.org/2000/svg" 
+                                        width="1em" 
+                                        height="1em" 
+                                        viewBox="0 0 1024 1024"
                                     >
-                                    </path>
-                                </svg>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="p-4">1</td>
-                            <td class="p-4">2</td>
-                            <td class="p-4">3</td>
-                            <td class="p-4">
+                                        <path 
+                                            fill="currentColor" 
+                                            d="M128 544h768a32 32 0 1 0 0-64H128a32 32 0 0 0 0 64z"
+                                        >
+                                        </path>
+                                    </svg>
+                                </td>
+                            </tr>
+                            <tr>
+                            <td class="p-4 cursor-pointer" v-on:click="getNumber('1')">1</td>
+                            <td class="p-4 cursor-pointer" v-on:click="getNumber('2')">2</td>
+                            <td class="p-4 cursor-pointer" v-on:click="getNumber('3')">3</td>
+                            <td class="p-4 cursor-pointer" v-on:click="processOutput('add')">
                                 <svg 
                                     xmlns="http://www.w3.org/2000/svg" 
                                     width="1em" 
@@ -111,18 +111,18 @@
                                     </path>
                                 </svg>
                             </td>
-                          </tr>
-                          <tr>
-                            <td class="p-4">.</td>
-                            <td class="p-4" colspan="2">
+                            </tr>
+                            <tr>
+                            <td class="p-4 cursor-pointer" v-on:click="getDot()">.</td>
+                            <td class="p-4 cursor-pointer" colspan="2" v-on:click="getNumber('0')">
                                0
                             </td>
-                            <td class="p-4">
+                            <td class="p-4 cursor-pointer" v-on:click="updateOutput()">
                                 =
                             </td>
-                          </tr>
+                            </tr>
                         </tbody>
-                      </table>
+                    </table>
                 </div>
             </div>
         </div>
@@ -141,6 +141,63 @@
         'IcBaselinePlusMinusAlt',
         'GgMathDivide'
     ],
+    data(){
+        return {
+            output: '',
+            previousValue: null,
+            operationFired: false,
+        }
+    },
+    methods: {
+        clearField() {
+            this.output = '';
+        },
+        setNegativeOrPositive() {
+            this.output = this.output[0] === '-' ? this.output.slice(1) : `-${this.output}`;
+        },
+        calculatePercentage() {
+            this.output = parseFloat(this.output)/100;
+        },
+        getNumber(number) {
+            if (this.operationFired) {
+                this.output = '';
+                this.operationFired = false;
+            }
+
+            this.output = `${this.output}${number}`
+        },
+        getDot() {
+            if (this.output.indexOf('.') === -1) {
+                this.output = this.output+'.'
+            }
+        },
+        processOutput(string) {
+            if (string == 'add') {
+                this.operation = (a, b) => {
+                    return parseFloat(a) + parseFloat(b)
+                }
+            } else if (string == 'subtract') {
+                this.operation = (a, b) => {
+                    return parseFloat(a) - parseFloat(b)
+                }
+            } else if (string == 'divide') {
+                this.operation = (a, b) => {
+                    return parseFloat(a) / parseFloat(b)
+                }
+            } else if (string == 'multiply') {
+                this.operation = (a, b) => {
+                    return parseFloat(a) * parseFloat(b)
+                }
+            }
+         
+            this.previousValue = this.output;
+            this.operationFired = true      
+        },
+        updateOutput() {
+            this.output = `${this.operation(this.previousValue, this.output)}`;
+            this.previousValue = null;
+        }
+    }
   }
 </script>  
 
@@ -152,5 +209,9 @@
 .output {
     background-color: aquamarine;
     color: black;
+}
+
+.cursor-pointer {
+    cursor: pointer;
 }
 </style>
